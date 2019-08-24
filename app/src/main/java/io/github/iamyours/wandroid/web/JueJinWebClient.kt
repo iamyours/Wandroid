298725429bf51d2cafb82660eda4caa9e45919bb
严星星 <353518643@qq.com>
2019-08-24 16:19:42 +0800

    掘金头像bug

--- app/src/main/java/io/github/iamyours/wandroid/web/JueJinWebClient.kt
+++ app/src/main/java/io/github/iamyours/wandroid/web/JueJinWebClient.kt
@@ -5,11 +5,17 @@ import android.util.Log
 import android.webkit.WebResourceResponse
 import android.webkit.WebView
 import androidx.lifecycle.MutableLiveData
+import com.google.gson.Gson
+import com.google.gson.JsonObject
+import okhttp3.*
+import java.io.IOException
 
 /**
  * 掘金app适配
  */
 class JueJinWebClient(vo: MutableLiveData<Boolean>) : BaseWebViewClient(vo) {
+    var detailApi = ""
+
     val script = """
         javascript:(function(){
             var arr = document.getElementsByClassName("lazyload");
@@ -17,20 +23,65 @@ class JueJinWebClient(vo: MutableLiveData<Boolean>) : BaseWebViewClient(vo) {
                 var img = arr[i];
                 var src = img.getAttribute("data-src");
                 img.src = src;
+
             }
         })();
     """.trimIndent()
     var load = false
     val handler = Handler()
+    val juejinUrl = "https://juejin.im/post/"
     override fun onPageFinished(view: WebView?, url: String?) {
         super.onPageFinished(view, url)
-        if ((url ?: "").startsWith("https://juejin.im/post/")) {
+        if ((url ?: "").startsWith(juejinUrl)) {
             if (load) return
+            val postId = url?.substring(juejinUrl.length) ?: ""
+            detailApi = getDetailApi(postId)
             handler.postDelayed({
                 view?.loadUrl(script)
                 load = true
-            },100)
+                loadUser(view!!)
+            }, 100)
         }
+    }
+
+    private fun loadUser(webView: WebView) {
+        val client = OkHttpClient.Builder().build()
+        val req = Request.Builder().url(detailApi).build()
+        val call = client.newCall(req)
+        call.enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val res = response.body()?.string() ?: "{}"
+                val obj =
+                    Gson().fromJson<JsonObject>(res, JsonObject::class.java)
+                updateUser(obj, webView)
+            }
+        })
+    }
+
+    private fun updateUser(obj: JsonObject?, webView: WebView) {
+        val user = obj?.getAsJsonObject("d")
+            ?.getAsJsonObject("user")
+        user?.run {
+            val head = get("avatarLarge").asString
+            val username = get("username").asString
+            val script = """
+                javascript:(function(){
+                    document.getElementsByClassName("author-info-block")[0].children[0].children[0].style.backgroundImage = "url('$head')";
+                    document.getElementsByClassName("username")[0].innerHTML="$username";
+                })();
+            """.trimIndent()
+            handler.post {
+                webView.loadUrl(script)
+            }
+        }
+    }
+
+    private fun getDetailApi(postId: String): String {//头像没有加载，手动调用
+        return "https://post-storage-api-ms.juejin" +
+                ".im/v1/getDetailData?src=web&type=entry&postId=$postId"
     }
 
     override fun shouldInterceptRequest(view: WebView?, url: String?)
