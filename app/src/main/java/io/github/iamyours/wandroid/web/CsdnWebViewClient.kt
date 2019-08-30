a823f6bdc102553319c55323c66e8418ec44097c
严星星 <353518643@qq.com>
2019-08-30 14:06:43 +0800

    csdn文章暗黑模式适配

---
+++ app/src/main/java/io/github/iamyours/wandroid/web/CsdnWebViewClient.kt
@@ -0,0 +1,33 @@
+package io.github.iamyours.wandroid.web
+
+import android.util.Log
+import android.webkit.WebResourceResponse
+import android.webkit.WebView
+import androidx.lifecycle.MutableLiveData
+
+class CsdnWebViewClient(url: String, vo: MutableLiveData<Boolean>) :
+    BaseWebViewClient(url, vo) {
+    override fun shouldInterceptRequest(view: WebView?, url: String?)
+            : WebResourceResponse? {
+        Log.i("csdn", "url:$url")
+        val urlStr = url ?: ""
+        if (urlStr.endsWith(".css")) {
+            if (urlStr.startsWith("https://csdnimg.cn/release/phoenix/production/mobile_detail_style")
+
+            ) {
+                val stream =
+                    view!!.context.assets.open("csdn/mobile_detail.css")
+                return WebResourceResponse("text/css", "utf-8", stream)
+            }
+            if (urlStr.startsWith("https://csdnimg.cn/release/phoenix/production/wapedit_views_md")
+
+            ) {
+                val stream =
+                    view!!.context.assets.open("csdn/md.css")
+                return WebResourceResponse("text/css", "utf-8", stream)
+            }
+        }
+
+        return super.shouldInterceptRequest(view, url)
+    }
+}
--- app/src/main/java/io/github/iamyours/wandroid/web/WebViewClientFactory.kt
+++ app/src/main/java/io/github/iamyours/wandroid/web/WebViewClientFactory.kt
@@ -8,12 +8,14 @@ object WebViewClientFactory {
     val JIAN_SHU = "https://www.jianshu.com"
     val JUE_JIN = "https://juejin.im/post/"
     val WEI_XIN = "https://mp.weixin.qq.com/s"
+    val CSDN = "https://blog.csdn.net"
     fun create(url: String, vo: MutableLiveData<Boolean>): WebViewClient {
         return when {
             url.contains(WAN_ANDROID) -> WanAndroidWebClient(url, vo)
             url.startsWith(JIAN_SHU) -> JianShuWebClient(url, vo)
             url.startsWith(JUE_JIN) -> JueJinWebClient(url, vo)
             url.startsWith(WEI_XIN) -> WeiXinWebClient(url, vo)
+            url.startsWith(CSDN) -> CsdnWebViewClient(url, vo)
             else -> object : BaseWebViewClient(url, vo) {}
         }
     }
